4fb30224e599c9196f056a6c8bdeaf1e784171a4
transientCloud <supr.subr@gmail.com>
2021-03-02 20:07:39 -0600

    added programming project-1

--- teaching/cs3640/assignments/web-server.py
+++ teaching/cs3640/assignments/web-server.py
@@ -8,46 +8,46 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 #FillInStart 
 #FillInEnd 
 
-while True: 	
-	print('Ready to serve...') 
-	#Set up a new connection from the client
-	connectionSocket, addr = serverSocket.accept()
+while True:     
+    print('Ready to serve...') 
+    #Set up a new connection from the client
+    connectionSocket, addr = serverSocket.accept()
 
-	#If an exception occurs during the execution of try clause
-	#the rest of the clause is skipped
-	#If the exception type matches the word after except
-	#the except clause is executed
-	try: 
-		#Receive the request message from the client
-		message = #FillInStart #FillInEnd 
-		
+    #If an exception occurs during the execution of try clause
+    #the rest of the clause is skipped
+    #If the exception type matches the word after except
+    #the except clause is executed
+    try: 
+        #Receive the request message from the client
+        message = #FillInStart #FillInEnd 
+        
         #Extract the path of the requested object from the message
-		#The path is the second part of HTTP header, identified by [1]
-		filename = message.split()[1] 
-		#Because the extracted path of the HTTP request includes 
-		#a character '\', we read the path from the second character 
-        f = open(filename[1:]) 		
-		#Store the entire content of the requested file in a buffer
+        #The path is the second part of HTTP header, identified by [1]
+        filename = message.split()[1] 
+        #Because the extracted path of the HTTP request includes 
+        #a character '\', we read the path from the second character 
+        f = open(filename[1:])      
+        #Store the entire content of the requested file in a buffer
         outputdata = f.read()
-		
-		#Send the HTTP response header line to the connection socket
-		#FillInStart 		
+        
+        #Send the HTTP response header line to the connection socket
+        #FillInStart        
         #FillInEnd
 
-		#Send the content of the requested file to the client 
-		for i in range(0, len(outputdata)): 
-			connectionSocket.send(outputdata[i].encode()) 				
-		
+        #Send the content of the requested file to the client 
+        for i in range(0, len(outputdata)): 
+            connectionSocket.send(outputdata[i].encode())               
+        
         connectionSocket.send("\r\n".encode()) 
-		connectionSocket.close() 
-	
-	except IOError:
-		#Send HTTP response message for file not found
-    	#FillInStart
-    	#FillInEnd 
-		
-		#Close client socket 
-		connectionSocket.close()
+        connectionSocket.close() 
+    
+    except IOError:
+        #Send HTTP response message for file not found
+        #FillInStart
+        #FillInEnd 
+        
+        #Close client socket 
+        connectionSocket.close()
 
 #Terminate the program
 serverSocket.close() 
